573f643252bd8b357f3e64f891ac123b24a9b32a
Mario Fernando Gonsalvez Arias <mariofernandogonsalvez@gmai.com>
2024-04-27 13:42:47 -0500

    Mejoras

--- lib/components/routes/views/services/maintenance.dart
+++ lib/components/routes/views/services/maintenance.dart
@@ -24,21 +24,6 @@ class _MaintenanceServiceState extends State<MaintenanceService> {
   final TextEditingController tipeController = TextEditingController();
   final TextEditingController desmaController = TextEditingController();
 
-  /*String _servicio = '';
-  String _cliente = '';
-  String _tipo = '';
-  String _descripcion = '';
-  String _tecnico = '';
-  String _restecnico = '';
-  String _etapa = '';
-  String _tectotal = '';
-  String _extras = '';
-  String _total = '';
-  String _fsolicitud = '';
-  String _hsolicitud = '';
-  String _frespuesta = '';
-  String _hrespuesta = '';*/
-
   final _pref = PreferencesUser();
 
   String mantenimientoTag = 'mantenimientoTag';
@@ -180,6 +165,151 @@ class _MaintenanceServiceState extends State<MaintenanceService> {
     );
   }
 
+  void details_Mantenimiento(String id) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Llene todos los campos'),
+          icon: const Icon(Icons.build),
+          shadowColor: WallpaperColor.baliHai().color,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          content: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance
+                  .collection('Servicios')
+                  .doc(id)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const AlertDialog(
+                    title: Text('Algo salio mal'),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  );
+                }
+                if (snapshot.data == null) {
+                  return const Text('No hay datos');
+                }
+                final doc = snapshot.data!;
+
+                final TextEditingController tipoController =
+                    TextEditingController(text: doc['tipo']);
+                final TextEditingController descripcionController =
+                    TextEditingController(text: doc['descripcion']);
+                final TextEditingController tecnicosController =
+                    TextEditingController(text: doc['tecnico']);
+                final TextEditingController restecnicosController =
+                    TextEditingController(text: doc['restecnico']);
+                final TextEditingController etapaController =
+                    TextEditingController(text: doc['etapa']);
+                final TextEditingController tectotalController =
+                    TextEditingController(text: doc['tectotal']);
+                final TextEditingController extrasController =
+                    TextEditingController(text: doc['extras']);
+                final TextEditingController totalController =
+                    TextEditingController(text: doc['total']);
+                final TextEditingController fsolicitudController =
+                    TextEditingController(text: doc['fsolicitud']);
+                final TextEditingController hsolicitudController =
+                    TextEditingController(text: doc['hsolicitud']);
+                final TextEditingController frespuestaController =
+                    TextEditingController(text: doc['frespuesta']);
+                final TextEditingController hrespuestaController =
+                    TextEditingController(text: doc['hrespuesta']);
+
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        MyTextField(
+                          labelText: 'Tipo de computador',
+                          obscureText: false,
+                          controller: tipoController,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        MyTextField(
+                          labelText: 'Decripcion del daño de su equipo',
+                          obscureText: false,
+                          controller: descripcionController,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(16, 5, 16, 0),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.asset(
+                              Theme.of(context).brightness == Brightness.light
+                                  ? 'assets/14.png'
+                                  : 'assets/13.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF8894B2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      tipeController.clear();
+                      desmaController.clear();
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancelar',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF07529B),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      GuardarMantenimiento();
+                      tipeController.clear();
+                      desmaController.clear();
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Solicitar',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+      barrierDismissible: false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final collections = GetCollectionsServices();
@@ -201,10 +331,15 @@ class _MaintenanceServiceState extends State<MaintenanceService> {
             appBar: AppBar(
               elevation: 0,
               title: const Center(child: Text('Mantenimiento')),
-              actions: const [
-                SizedBox(
-                  width: 48,
-                )
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    new_Mantenimiento();
+                  },
+                  tooltip: 'Add',
+                  alignment: Alignment.center,
+                ),
               ],
               backgroundColor: Theme.of(context).brightness == Brightness.light
                   ? WallpaperColor.steelBlue().color
@@ -212,17 +347,6 @@ class _MaintenanceServiceState extends State<MaintenanceService> {
             ),
             drawer: const MyDrawer(),
             backgroundColor: Theme.of(context).colorScheme.background,
-            floatingActionButton: FloatingActionButton(
-              heroTag: mantenimientoTag,
-              onPressed: () {
-                new_Mantenimiento();
-              },
-              backgroundColor: Theme.of(context).brightness == Brightness.light
-                  ? WallpaperColor.danube().color
-                  : WallpaperColor.baliHai().color,
-              shape: const CircleBorder(),
-              child: const Icon(Icons.add),
-            ),
             body: const Stack(
               children: [
                 Positioned.fill(
@@ -242,10 +366,15 @@ class _MaintenanceServiceState extends State<MaintenanceService> {
           appBar: AppBar(
             elevation: 0,
             title: const Center(child: Text('Mantenimiento')),
-            actions: const [
-              SizedBox(
-                width: 48,
-              )
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  new_Mantenimiento();
+                },
+                tooltip: 'Add',
+                alignment: Alignment.center,
+              ),
             ],
             backgroundColor: Theme.of(context).brightness == Brightness.light
                 ? WallpaperColor.steelBlue().color
@@ -253,92 +382,112 @@ class _MaintenanceServiceState extends State<MaintenanceService> {
           ),
           drawer: const MyDrawer(),
           backgroundColor: Theme.of(context).colorScheme.background,
-          floatingActionButton: FloatingActionButton(
-            heroTag: mantenimientoTag,
-            onPressed: () {
-              new_Mantenimiento();
-            },
-            backgroundColor: Theme.of(context).brightness == Brightness.light
-                ? WallpaperColor.danube().color
-                : WallpaperColor.baliHai().color,
-            shape: const CircleBorder(),
-            child: const Icon(Icons.add),
-          ),
           body: ListView.builder(
             itemCount: service?.length,
             itemBuilder: (context, index) {
               DocumentSnapshot document = service![index];
               Map<String, dynamic> data =
                   document.data() as Map<String, dynamic>;
+              String docID = document.id;
 
-              return Container(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
-                color: Colors.transparent,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(5),
-                          child: Image.asset(
-                            data['restecnico'] == ''
-                                ? 'assets/ojo_cerrado.png'
-                                : 'assets/ojo_abierto.png',
-                            width: 50,
-                            height: 50,
-                            color:
-                                Theme.of(context).brightness == Brightness.light
-                                    ? WallpaperColor.black().color
-                                    : WallpaperColor.white().color,
-                            errorBuilder: (context, error, stackTrace) {
-                              return IconButton(
-                                highlightColor: Colors.transparent,
-                                onPressed: () {},
-                                icon: Image.asset(
-                                  'assets/user.png',
-                                  width: 121.8,
-                                  height: 121.8,
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                data['tipo'] ?? '',
-                                style: const TextStyle(
-                                    fontSize: 30, fontWeight: FontWeight.bold),
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                child: Container(
+                  width: MediaQuery.of(context).size.height * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Theme.of(context).brightness == Brightness.light
+                        ? WallpaperColor.veniceBlue().color
+                        : WallpaperColor.iceberg().color,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Image.asset(
+                                data['restecnico'] == ''
+                                    ? 'assets/ojo_cerrado.png'
+                                    : 'assets/ojo_abierto.png',
+                                width: 50,
+                                height: 50,
+                                color: Theme.of(context).brightness ==
+                                        Brightness.light
+                                    ? WallpaperColor.white().color
+                                    : WallpaperColor.black().color,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return IconButton(
+                                    highlightColor: Colors.transparent,
+                                    onPressed: () {},
+                                    icon: Image.asset(
+                                      'assets/user.png',
+                                      width: 121.8,
+                                      height: 121.8,
+                                    ),
+                                  );
+                                },
                               ),
-                              Text(
-                                data['descripcion'] ?? '',
-                                style: const TextStyle(fontSize: 18),
-                              )
-                            ],
+                            ),
                           ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(20, 10, 30, 0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  data['tipo'] ?? '',
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .primary),
+                                ),
+                                Text(
+                                  data['descripcion'] ?? '',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .primary),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.info),
+                              onPressed: () {
+                                details_Mantenimiento(docID);
+                              },
+                              iconSize: 50,
+                              tooltip: 'Add',
+                              color: Theme.of(context).brightness ==
+                                      Brightness.light
+                                  ? WallpaperColor.viking().color
+                                  : WallpaperColor.blueZodiac().color,
+                              alignment: Alignment.center,
+                            ),
+                            Text(
+                              'Detalles',
+                              style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary),
+                            )
+                          ],
                         ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.info),
-                          onPressed: () {
-                            // Add your onPressed logic here
-                          },
-                          iconSize: 50,
-                          tooltip: 'Add',
-                          alignment: Alignment.center,
-                        ),
-                        const Text('Detalles')
-                      ],
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               );
             },
